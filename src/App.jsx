bba0753af2c54cf6b80bcc8c054d1e3f9951efa6
momin096 <momin09609@gmail.com>
2025-01-14 00:41:26 +0600

    add bg

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Countries from './component/Countries/Countries'
+
+function App() {
+
+  return (
+    <>
+      
+      <h2>React World Tour</h2>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App;
---
+++ src/component/Countries/Countries.jsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Country from "../country/country";
+import './Countries.css'
+const Countries = () => {
+    const [countries, setCountries] = useState([]);
+
+    useEffect(() =>{
+        fetch('https://restcountries.com/v3.1/all')
+        .then(res => res.json())
+        .then(data => setCountries(data));
+    },[])
+    return (
+        <div className="country-container">
+            {
+                countries.map(country => <Country key={country.cca3} country={country}></Country>)
+            }
+        </div>
+    );
+};
+
+export default Countries;
